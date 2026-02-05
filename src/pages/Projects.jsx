ad171e6af5c5519f78ee73d9da9fb07dd0adad07
Mirfeez <mahammadmirfeez@gmail.com>
2026-02-05 15:50:27 +0530

    Merge branch 'main' of https://github.com/Mirfeez/DigiPixelTech

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,29 +1,50 @@
 import React from "react";
 
 const Projects = () => {
-   return (
-      <>
-      <div className="bg-[url('img/banner/Project.png')] w-full h-150 bg-cover flex flex-col justify-center leading-normal">
-         <h1 className="text-5xl">Discover the Projects </h1>
-            <h1 className="text-5xl">that we have worked</h1>
-            <p>An overview of the projects we’ve designed and developed.</p>
-      </div>
-      <div></div>
-      <div>
-         <h1 className="my-20 text-5xl flex justify-center">Projects</h1>
-         <div className="flex flex-row">
-            <div className="w-3/5 h-210">
-            <iframe src="https://haseeb-interior.netlify.app/" frameborder="0" className="iframe h-200 w-400 m-10"></iframe>
-            </div>
-            <div className="w-1/3 mt-8 mr-10">
-               <h1 className="text-3xl mb-5">Interior Portfolio</h1>
-               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem cum ea, odio quibusdam nulla amet culpa dolores ratione fugiat aperiam ipsa accusamus fuga vel mollitia neque nemo commodi fugit tenetur libero! Deserunt dolor minus beatae tempora atque facilis ea recusandae, porro maxime. Magni architecto deserunt aspernatur quo sit assumenda. Earum quas ad velit aliquam odit asperiores quo et! Ipsa corporis voluptate veniam, id repudiandae adipisci quam rem odio eum sed nesciunt natus quas earum omnis consequatur suscipit facere beatae voluptates sunt impedit blanditiis. Nostrum impedit recusandae, assumenda beatae vitae, laboriosam eaque, iure corporis ducimus quae similique maxime? At, recusandae quo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt tenetur hic provident quisquam dignissimos debitis adipisci labore facilis libero ea, ex vero nihil aspernatur facere porro doloremque sequi. Minima, architecto? Fugiat aut labore repellendus modi rerum exercitationem tenetur reiciendis ipsum dignissimos, excepturi dicta obcaecati neque, atque quo id voluptatem magni vero ut cumque sunt fuga magnam soluta. Nisi tempora eos totam esse, asperiores quaerat magni, ratione id, voluptatibus amet et aut aliquid deleniti molestiae sapiente aliquam. Quia aut corrupti cumque, asperiores ex voluptatem enim at repellat porro dolores dolore saepe cum, doloremque veniam facere nulla! Veritatis asperiores tenetur eum voluptas?</p>
-            </div>
-         </div>
-      </div>
-      <div></div>
-      </>
-   );
-}
+	return (
+		<>
+			<div className="bg-img-projects bg-[url('img/banner/Project.png')] font-serif w-full h-150 bg-cover flex flex-col justify-center leading-normal gap-2">
+				<h1 className="ml-10 text-5xl">Discover the Projects </h1>
+				<h1 className="ml-10 text-3xl">that we have worked</h1>
+				<p className="ml-10">
+					An overview of the projects which successfully designed and
+					developed.
+				</p>
+			</div>
+
+			<div className="font-serif">
+				<h1 className="mt-10 ml-10 text-5xl flex">Projects:-</h1>
+				<h2 className="mt-10 ml-10 text-3xl">1. Portfolio Website... </h2>
+				<h3 className="ml-10 text-xl">for an Interior Designer</h3>
+
+				<div className="m-5">
+					<div>
+						<iframe
+							src="https://haseeb-interior.netlify.app/"
+							frameborder="0"
+							className="iframe justify-self-center w-[60vw] h-[65vh]"
+						></iframe>
+					</div>
+					<div className="w-full text-center mt-8 mr-10">
+						<h1 className="text-3xl mb-5">Info:</h1>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Exercitationem cum ea, odio quibusdam nulla amet culpa
+							dolores ratione fugiat aperiam ipsa accusamus fuga vel
+							mollitia neque nemo commodi fugit tenetur libero! Deserunt
+							dolor minus beatae tempora atque facilis ea recusandae,
+							porro maxime. Magni architecto deserunt aspernatur quo sit
+							assumenda. Earum quas ad velit aliquam odit asperiores quo
+							et! Ipsa corporis voluptate veniam, id repudiandae adipisci
+							quam rem odio eum sed nesciunt natus quas earum omnis
+							consequatur suscipit facere beatae voluptates sunt impedit
+							
+						</p>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
 
 export default Projects;
